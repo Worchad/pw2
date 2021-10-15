f1b61f076e27e78db3b91c867556a8a013cc11f6
Angel Colman <angelcolman@pop-os.localdomain>
2021-10-15 20:43:16 -0300

    Construcción del backend para la recepción de datos enviados en el formulario html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     titulo = 'Bievenido'
     return render_template('login.html',titulo=titulo)
+
+@app.route('/procesar_credenciales', methods = ['POST'])
+def procesarCredenciales():
+    if request.method=='POST':
+        datos = request.form
+        correo = datos['txtCorreo']
+        password = datos['txtPassword']
+        return "Mi correo es: "+password
 
 if __name__ == '__main_':
     app.run(port=5000, debug=True)

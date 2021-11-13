4d4af27d10966eb3b91814b88cc5d4d233658dc8
Angel Colman <angelcolman@pop-os.localdomain>
2021-11-13 11:18:57 -0300

    Construccion de interfaz y logica de ABM

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,redirect,url_for,render_template,request,flash
+from flask import Flask,redirect,url_for,render_template,request,flash,json
 from config import ConfiguracionDesarrollo
 from model import db, Usuarios
+from sqlalchemy.ext.declarative import DeclarativeMeta
 
 app=Flask(__name__)
 app.config.from_object(ConfiguracionDesarrollo)
@@ -32,7 +33,83 @@ def procesarCredenciales():
 @app.route('/principal')
 def principal():
     return render_template('index.html')
+
+@app.route('/obtener_usuarios', methods=['POST'])
+def obtenerUsuarios():
+    if request.method == 'POST':
+        datos = request.form
+        codigo = datos['cod']
+        if codigo == "":
+            usuarios = Usuarios.query.all()
+        else:
+            usuarios = Usuarios.query.filter_by(usua_id = codigo).all()
     
+    return json.dumps(usuarios, cls=AlchemyEncoder)
+
+
+@app.route('/alta_usuarios', methods=['POST'])
+def altaUsuarios():
+    mensajes = ""
+    if request.method == 'POST':
+        datos = request.form
+        correo = datos['txtCorreo']
+        password = datos['txtPassword']
+        if(correo=="" or password==""):
+            mensajes = 'Debe completar los campos'
+        else:
+            registro = Usuarios(usua_correo=correo, usua_password=password)
+            db.session.add(registro)
+            db.session.commit()
+            mensajes = 'Usuario ha sido registrado correctamente'
+    return json.dumps(mensajes)
+
+@app.route('/modificacion_usuarios', methods=['POST'])
+def modificacionUsuarios():
+    mensajes = ""
+    if request.method == 'POST':
+        datos = request.form
+        codigo = datos['txtCodigo']
+        correo = datos['txtCorreo']
+        password = datos['txtPassword']
+        if(correo == "" or password == "" or codigo == ""):
+            mensajes = 'Debe completar los campos y/o seleccionar al usuario'
+        else:
+            modificacion = Usuarios.query.filter_by(usua_id=codigo).first()
+            modificacion.usua_correo = correo
+            modificacion.usua_password = password
+            db.session.commit()
+            mensajes = 'Usuario ha sido modificado correctamente'
+    return json.dumps(mensajes)
+
+@app.route('/baja_usuarios', methods=['POST'])  
+def bajaUsuarios():
+    mensajes = ""
+    if request.method == 'POST':
+        datos = request.form
+        codigo = datos['cod']
+        if(codigo == ""):
+            mensajes = 'Debe seleccionar al usuario'
+        else:
+            eliminar = Usuarios.query.filter_by(usua_id=codigo).first()
+            db.session.delete(eliminar)
+            db.session.commit()
+            mensajes = 'Usuario ha sido eliminado correctamente'
+    return json.dumps(mensajes)
+
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data)
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            return fields
+        return json.JSONEncoder.default(self,obj)
+
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
